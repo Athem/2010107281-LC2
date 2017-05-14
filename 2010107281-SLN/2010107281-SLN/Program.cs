9c0c353395a3e3ac954aec78083b29f490831ea9
unknown <gerardyerba@gmail.com>
2017-05-14 15:25:19 -0500

    Primer Commit

---
+++ 2010107281-SLN/2010107281-SLN/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2010107281_SLN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
